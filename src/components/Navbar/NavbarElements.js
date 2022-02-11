6f0b625fe1b3a46ff4089caea0ed1248de7b4374
meyouv237 <95403916+tulolo287@users.noreply.github.com>
2022-02-11 13:25:42 +0000

    header button

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -18,3 +18,60 @@ export const NavLogo = styled(LinkR)`
 color: red;
 text-decoration: none;
 `
+
+export const MobileIcon = styled.div`
+display: none;
+color: white;
+
+@media screen and (max-width: 768px) {
+  display: block;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: translate(-100%, 60%);
+}
+`
+
+export const NavMenu = styled.ul`
+display: flex;
+list-style: none;
+align-items: center;
+
+@media screen and (max-width: 768px) {
+  display: none;
+}
+`
+
+export const NavItem = styled.li`
+height: 80px;
+`
+export const NavLinks = styled(LinkR)`
+display: flex;
+padding: 0 1rem;
+cursor: pointer;
+hight: 100%;
+color: #fff;
+text-decoration: none;
+align-items: center;
+
+&.active {
+  border-bottom: 3px solid green;
+}
+`
+export const NavBtn = styled.nav`
+align-items: center;
+
+@media screen and (max-width: 768px) {
+  display: none;
+}
+`
+
+export const NavBtnLink = styled(LinkR)`
+border-radius: 50px;
+background-color: green;
+color: #fff;
+padding: 10px 20px;
+font-size: 16px;
+text-decoration: none;
+transition: all 0.2s ease-in-out;
+`
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,27 @@
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from "./NavbarElements";
+import { FaBars } from 'react-icons/fa'; 
+
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">Logo</NavLogo>
+          <MobileIcon>
+            <FaBars/>
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="/catalog">Catalog</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/about">About</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn><NavBtnLink to="/signin">Sign in</NavBtnLink></NavBtn>
         </NavbarContainer>
       </Nav>
     </>
