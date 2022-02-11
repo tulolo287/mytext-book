8ed401cd016fec210c7aab1bf53d118154e4dc56
tulolo287 <95403916+tulolo287@users.noreply.github.com>
2022-02-11 07:44:34 +0000

    styled navbar

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { Link as LinkR } from "react-dom";
+
+export const Nav = styled.nav`
+  background: #000;
+  height: 80px;
+  position: sticky;
+`;
+
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  width: 100%;
+`;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <a href="/">Logo</a>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+export default Navbar;
