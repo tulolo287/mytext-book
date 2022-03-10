26c806e56232f7a4ab3e79bc368138c3ffcf0175
tulolo287 <iv1055@yandex.ru>
2022-03-10 16:35:44 +0300

    make signin component

---
+++ src/components/Signin/SigninElements.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+
+`
+export const FormWrap = styled.div`
+
+`
+export const Icon = styled.div`
+
+`
+export const FormContent = styled.div`
+
+`
+export const Form = styled.div`
+
+`
+export const FormH1 = styled.h1`
+
+`
+export const FormLabel = styled.div`
+
+`
+export const FormButton = styled.div`
+
+`
+export const FormInput = styled.div`
+
+`
+export const Text = styled.div`
+
+`
--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,6 +1,23 @@
+import {Container, FormLabel, FormWrap, FormContent, Form, FormH1, FormInput, FormButton} from './SigninElements';
+
 const Signin = () => {
     return (<>
-    fsdfdsff
+    <Container>
+        <FormWrap>
+            <Icon></Icon>
+            <FormContent>
+                <Form action='#'>
+                    <FormH1>Sign in</FormH1>
+                    <FormLabel htmlFor='for'>Email</FormLabel>
+                    <FormInput type='email' required/>
+                    <FormLabel htmlFor='for'>Password</FormLabel>
+                    <FormInput type='password' required />
+                    <FormButton type='submit'>Continue</FormButton>
+                    <Text>Forgot password</Text>
+                </Form>
+            </FormContent>
+        </FormWrap>
+    </Container>
     </>)
 }
 export default Signin;
