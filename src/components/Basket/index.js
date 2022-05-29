6084ab62bd8d3a8ad1ea8853370007eadeffcf1a
Tolo <iv1055@yandex.ru>
2022-05-29 20:22:28 +0000

    cart item3

--- src/components/Basket/index.js
+++ src/components/Basket/index.js
@@ -8,6 +8,11 @@ export default function Basket(props) {
     <div>
       {cartItems.length == 0 && <div>Cart is empty...</div>}
     </div>
+    {cartItems.map(item => 
+    <div key={item.id} className='row'>
+      <div className='col-2'>{item.title}</div>
+      <button onClick={onAdd}>+</button>
+    </div>)}
     </aside>
   )
 }
