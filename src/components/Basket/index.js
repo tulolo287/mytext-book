b4d7de03e7c5fd90e1af22f232714fd41d26f144
tulolo287 <iv1055@yandex.ru>
2022-05-30 16:36:51 +0300

    lhlkj22

--- src/components/Basket/index.js
+++ src/components/Basket/index.js
@@ -1,12 +1,26 @@
 
 
 export default function Basket(props) {
-  const {cartItems, onAdd} = props;
+  const {cartItems, onAdd, onRemove} = props;
 
   return (
     <aside className="block col-1"><h2>Cart Items</h2>
     <div>
       {cartItems.length == 0 && <div>Cart is empty...</div>}
+      {cartItems.map(item => (
+      <div key={item.id + item.title} className='row'>
+        <div>{item.title}</div>
+        <div>
+          <button onClick={() => onAdd(item)} className='add'>+</button>
+          <button onClick={() => onRemove(item)} className='remove'>-</button>
+        </div>
+        <div>
+        {item.qty} x ${item.price.toFixed(2)}
+        </div>
+      </div>
+      )
+      
+      )}
     </div>
     </aside>
   )
