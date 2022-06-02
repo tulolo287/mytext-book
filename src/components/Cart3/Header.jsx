d44bb1752b0ed7c1dc56e2c4e1953982dea23eaf
tulolo287 <iv1055@yandex.ru>
2022-06-02 16:37:19 +0300

    link go to cart

--- src/components/Cart3/Header.jsx
+++ src/components/Cart3/Header.jsx
@@ -1,12 +1,18 @@
 
-import { Badge, Nav, Navbar, Container, FormControl, Dropdown } from "react-bootstrap"
+import { Badge, Nav, Navbar, Container, FormControl, Dropdown, Button } from "react-bootstrap"
+import { AiFillDelete } from "react-icons/ai";
 import { FaShoppingCart } from "react-icons/fa"
+import { CartState } from "../../context/Context";
+import { Link } from "react-router-dom"
 
 
 
 export const Header = () => {
+
+  const { state: { cart }, dispatch } = CartState();
+
   return (
-    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+    <Navbar bg="dark" variant="dark" style={{ height: 80, marginBottom: 20 }}>
       <Container>
         <Navbar.Brand>
           <a href='/#'>Shopping cart</a>
@@ -15,17 +21,39 @@ export const Header = () => {
           <FormControl style={{ width: 500 }} placeholder='Search' className='m-auto'></FormControl>
         </Navbar.Text>
         <Nav>
-          <Dropdown alignRight>
+          <Dropdown>
             <Dropdown.Toggle variant='success'>
               <FaShoppingCart color='red' fontSize='25px' />
-              <Badge>{3}</Badge>
+              <Badge>{cart.length}</Badge>
             </Dropdown.Toggle>
             <Dropdown.Menu style={{ minWidth: 344 }}>
-              <span style={{ padding: 10 }}>Cart is empty</span>
+              {cart.length > 0 ? (
+                cart.map(product => (
+                  <>
+                    <div key={product.id} className="cartItem" >
+                      <img src={product.image} alt={product.name} className="cartItemImage" />
+                      <div className="cartItemDetail">
+                        <span>{product.name}</span>
+                        <span>{product.price.split(".")[0]}</span>
+
+                      </div>
+                      <AiFillDelete style={{ cursor: "pointer" }}
+                        fontSize="20px"
+                        onClick={() => dispatch({ type: "REMOVE_FROM_CART", payload: product })} />
+
+                      <hr />
+                    </div>
+                    <Link to="/cart_detail">
+                      <Button>Go to cart</Button>
+                    </Link>
+                  </>
+                ))
+              ) : (<span style={{ padding: 10 }}>Cart is empty</span>)}
+
             </Dropdown.Menu>
           </Dropdown>
         </Nav>
       </Container>
-    </Navbar>
+    </Navbar >
   )
 }
--- src/components/Cart3/Product.jsx
+++ src/components/Cart3/Product.jsx
@@ -1,11 +1,16 @@
 import { Button, Card } from "react-bootstrap";
+import { CartState } from "../../context/Context";
 import { Rating } from "./Rating";
 
 export const Product = ({ product }) => {
+  const { state: { cart }, dispatch } = CartState();
+
+  console.log(cart);
+
   return (
     <div className="products">
       <Card>
-        <Card.Img className="products" variant="top" alt={product.name} src={product.image} />
+        <Card.Img variant="top" alt={product.name} src={product.image} />
         <Card.Body>
           <Card.Title>{product.name}</Card.Title>
           <Card.Subtitle style={{ paddingBottom: 10 }}>
@@ -17,10 +22,13 @@ export const Product = ({ product }) => {
             )}
             <Rating rating={product.rating}></Rating>
           </Card.Subtitle>
-          <Button variant="danger">Remove from cart</Button>
-          <Button disabled={!product.stock}>{!product.stock ? "Out of stock" : "Add to cart"}</Button>
+          {cart.some(p => p.id === product.id) ?
+            (<Button variant="danger" onClick={() => dispatch({ type: "REMOVE_FROM_CART", payload: product })}>Remove from cart</Button>) :
+            (<Button disabled={!product.stock} onClick={() => dispatch({ type: "ADD_TO_CART", payload: product })}>{!product.stock ? "Out of stock" : "Add to cart"}</Button>)}
+
+
         </Card.Body>
       </Card>
-    </div>
+    </div >
   );
 };
